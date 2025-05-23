ca9042c607586ef26ec42ba107896ec4b0e4c21a
ZulkifliAhmed <zoalkeflabunday@gmail.com>
2025-05-23 15:42:07 +0300

    update

---
+++ js/list.js
@@ -0,0 +1,20 @@
+let allAudio = [
+    {
+        name : "first song",
+        artist : "first singer",
+        img : "images/office-1.png",
+        src : "audio/112.mp3"
+    },
+      {
+        name : "second song",
+        artist : "second singer",
+        img : "images/office-2.jpg",
+        src : "audio/113.mp3"
+    },
+      {
+        name : "third song",
+        artist : "third singer",
+        img : "images/office-3.jpg",
+        src : "audio/114.mp3"
+    }
+]
